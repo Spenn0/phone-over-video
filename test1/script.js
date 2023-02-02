109522b7983e3e9d1f017543834b8ed1dc9f46de
Spencer Brown <spencerbrown@Spencers-MacBook-Pro.local>
2023-02-02 16:35:18 -0600

    gitignore

--- test1/script.js
+++ test1/script.js
@@ -2,7 +2,17 @@ let video = document.querySelector("#genZ-video");
 let button = document.querySelector("#playButton");
 
 
-// video.addEventListener("click", playVideo());
+
+
+
+video.addEventListener("mouseover", (event) => {
+    if(event){
+        button.classList.add("#playButton");
+    }
+})
+
+
+
 button.addEventListener("click", () => {
     if(button.classList.contains("active") === true){
     video.play()
@@ -14,6 +24,8 @@ button.addEventListener("click", () => {
         button.classList.add("active")
     }
 })
+
+
 
 function playVideo() {
 
