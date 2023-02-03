b7868a4dafdf443fc9d5a5a9ab4cd73eabeddfde
Spencer Brown <spencerbrown@Spencers-MacBook-Pro.local>
2023-02-03 10:56:32 -0600

    finished the play and pause button functionality

--- test1/script.js
+++ test1/script.js
@@ -1,32 +1,56 @@
+// set query selector variables
 let video = document.querySelector("#genZ-video");
-let button = document.querySelector("#playButton");
+let playButton = document.querySelector("#playButton");
+let pauseButton = document.querySelector("#pauseButton");
+let container = document.querySelector("#container");
 
 
+//when user mouses into container and either button is active remove .fade class
+container.addEventListener("mouseover", (event) => {
+    if (event && playButton.classList.contains("active")){
+        playButton.classList.remove("fade")
+        } else if (event && pauseButton.classList.contains("active")){
+            pauseButton.classList.remove("fade")
+        }
+})
 
-
-
-video.addEventListener("mouseover", (event) => {
-    if(event){
-        button.classList.add("#playButton");
+//when user mouses out of container and either button is active - add .fade class
+container.addEventListener("mouseout", (event) => {
+    if(event && playButton.classList.contains("active")){
+        playButton.classList.add("fade")
+    } else if (event && pauseButton.classList.contains("active")){
+        pauseButton.classList.add("fade")
     }
 })
 
+// when user presses the play button
+// remove playButton from view
+// add pauseButton into view
+// play video
+// switch the active class on the buttons
+playButton.addEventListener("click", () => {
+    if(playButton.classList.contains("active") === true){
+        playButton.style.display = "none"
+        pauseButton.style.display = "block"
+        video.play()
+        console.log("PLAY VIDEO")
+        playButton.classList.remove("active")
+        pauseButton.classList.add("active");
+    } 
+})
 
-
-button.addEventListener("click", () => {
-    if(button.classList.contains("active") === true){
-    video.play()
-    console.log("PLAY VIDEO")
-    button.classList.remove("active")
-    } else{
+// when user presses the pause button
+// remove pauseButton from view
+// add playButton into view
+// pause video
+// switch the active class on the buttons
+pauseButton.addEventListener("click", () => {
+    if(pauseButton.classList.contains("active") === true){
+        pauseButton.style.display = "none"
+        playButton.style.display = "block"
         video.pause()
         console.log("PAUSE VIDEO")
-        button.classList.add("active")
+        pauseButton.classList.remove("active")
+        playButton.classList.add("active")
     }
 })
-
-
-
-function playVideo() {
-
-}
